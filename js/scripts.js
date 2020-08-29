4b8437c29b2c9b8768427123a3bbc0accf262e05
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-08-29 21:44:13 +0300

    add hide and show style in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,26 @@
-$(document).ready(function(){
-    $("#design-img1").click(function(){
-    $(".design-page1").hide(1000).toggle();
-       $("design-img1").show();
- 
-    });
-    $(".design-page1").click(function(){
-      $("#design-img1").show();
-      $(".design-page1").hide();
-    })
-    $("#product-img3").click(function(){
-      $(".product-page3").hide(1000).toggle();
-      $("#product-img3").show ();
-        });
-        $(".product-page3").click(function(){
-          $("#product-img3").show();
-          $(".product-page3").hide();
-        })
- 
-    $("#development-img2").click(function(){
-      $(".development-page2").hide(1000).toggle();
-      $("#development-img2").show();
-    });
-   $(".development-page2").click(function(){
-     $("#development-img2").show();
-     $(".development-page2").hide();
-   })
- 
- 
- 
-  })
+$(document).ready(function () {
+	$("#design-picture").click(function () {
+		$("#design-page1").show();
+		$("#design-picture").hide();
+	});
+	$("#design-page1").click(function () {
+		$("#design-page1").hide();
+		$("#design-picture").show();
+	});
+	$("#development-picture").click(function () {
+		$("#development-page2").show();
+		$("#development-picture").hide();
+	});
+	$("#development-page2").click(function () {
+		$("#development-page2").hide();
+		$("#development-picture").show();
+	});
+	$("#production-picture").click(function () {
+		$("#production-page3").show();
+		$("#production-picture").hide();
+	});
+	$("#production-page3").click(function () {
+		$("#production-page3").hide();
+		$("#production-picture").show();
+	});
+});
