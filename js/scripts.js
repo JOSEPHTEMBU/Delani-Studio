4191d613c4790037f4006c573900e104cd1801af
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-08-30 14:00:59 +0300

    add hover effects on images in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,36 @@ $(document).ready(function () {
 		$("#production-picture").show();
 	});
 });
+
+$(document).ready(function () {
+	$("#hover1").hover(function () {
+		$("#imgtext1").toggle();
+	});
+	$("#hover2").hover(function () {
+		$("#imgtext2").toggle();
+	});
+	$("#hover3").hover(function () {
+		$("#imgtext3").toggle();
+	});
+	$("#hover4").hover(function () {
+		$("#imgtext4").toggle();
+	});
+	$("#hover5").hover(function () {
+		$("#imgtext5").toggle();
+	});
+	$("#hover6").hover(function () {
+		$("#imgtext6").toggle();
+	});
+	$("#hover7").hover(function () {
+		$("#imgtext7").toggle();
+	});
+	$("#hover8").hover(function () {
+		$("#imgtext8").toggle();
+	});
+});
+function contact(form){
+	var name = document.forms["form2"]["name"].value;
+		var email = document.forms["form2"]["email"].value;
+		var message = document.forms["form2"]["message"].value;
+		alert("Hello! " + name + "," + " We have received your message. Thank you for reaching out to us. ");
+		  };
